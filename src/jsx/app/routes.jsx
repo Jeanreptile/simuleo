0c0ae7d3bfc0abda16f4a0400ee026ebbda9b0d5
Florian Magot <flo.magot@gmail.com>
2016-01-14 16:48:21 +0100

    Added static teacher's dashboard

--- src/jsx/app/routes.jsx
+++ src/jsx/app/routes.jsx
@@ -10,6 +10,8 @@ var simul_create = require('./routes/app/simul_create.jsx');
 var simul_model = require('./routes/app/simul_model.jsx');
 var simul_instance = require('./routes/app/simul_instance.jsx');
 var simul_instance_groups = require('./routes/app/simul_instance_groups.jsx');
+var simul_instance_dashboard = require('./routes/app/simul_instance_dashboard.jsx');
+var simul_instance_dashboard_end = require('./routes/app/simul_instance_dashboard_end.jsx');
 var signup = require('./routes/app/signup.jsx');
 var login = require('./routes/app/login.jsx');
 var logout = require('./routes/app/logout.jsx');
@@ -31,9 +33,12 @@ module.exports = (
     <Route path='/simul/setup' handler={simul_setup} />
     <Route path='/simul_instance' handler={simul_instance} />
     <Route path='/simul_instance/groups' handler={simul_instance_groups} />
+    <Route path='/simul_instance/dashboard' handler={simul_instance_dashboard} />
+    <Route path='/simul_instance/dashboard_end' handler={simul_instance_dashboard_end} />
     <Route path='/signup' handler={signup} />
     <Route path='/login' handler={login} />
     <Route path='/logout' handler={logout} />
+
     <NotFoundRoute handler={notfound} />
   </Route>
 );
